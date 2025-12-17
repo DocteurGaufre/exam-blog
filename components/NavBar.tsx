a643a52ba6947e07003acba8fd14310afa68c75a
DocteurGaufre <michaelspackitch@gmail.com>
2025-12-17 12:01:39 +0100

    Ajout d'une Navbar

---
+++ components/NavBar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { type ReactNode } from "react";
+
+type NavLinkProps = {
+    href: string;
+    children: ReactNode;
+    className?: string;
+};
+
+function NavLink({ href, children, className }: NavLinkProps) {
+    return (
+        <li className={className}>
+            <Link 
+                href={href} 
+                className="block py-2 px-4 hover:bg-[#609be2]"
+            >
+                {children}
+            </Link>
+        </li>
+    );
+}
+
+export default function Navbar() {
+    return (
+        <nav className="bg-[#4d7cb5] text-slate-50 shadow-xl sticky top-0 z-50">
+            <ul className="container mx-auto flex items-center">
+                <NavLink href="/">Accueil</NavLink>
+                <NavLink href="/cv">Page 1</NavLink>
+                <NavLink href="/blog">Page 2</NavLink>
+            </ul>
+        </nav>
+    );
+}
